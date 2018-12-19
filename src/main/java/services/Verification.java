ade01ab2e85b5b62ff12fb4477f92fda521702f2
DanielGara <yo@danielgara.com>
2018-12-19 11:30:32 -0500

    IMP: verification service test receiving mxgraph xml model

---
+++ src/main/java/services/Verification.java
@@ -0,0 +1,32 @@
+package services;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+@Controller
+public class Verification {
+	
+	@CrossOrigin
+	@RequestMapping(value="/Verification/test", method=RequestMethod.POST, produces="text/plain")
+	@ResponseBody
+	public String executeTest(@RequestBody String data_collected) {
+		JsonParser parser = new JsonParser();
+		JsonObject rootObj = parser.parse(data_collected).getAsJsonObject();
+		JsonElement data = rootObj.get("data");
+		String data_string = data.getAsString();
+		System.out.println("Data:");
+		System.out.println(data_string.substring(0,1000));
+		System.out.println(data_string.substring(0,data_string.length()));
+		System.out.println("End");
+		return "XML code received by the server";
+	}
+	
+}
