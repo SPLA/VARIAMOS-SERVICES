f753aea7cb2d0d179393d8f3bc89efcc830c7df2
DanielGara <yo@danielgara.com>
2020-03-14 12:04:53 -0500

    IMP: "Mysql integration"

---
+++ src/main/java/variamos/models/UserRepository.java
@@ -0,0 +1,9 @@
+package variamos.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
